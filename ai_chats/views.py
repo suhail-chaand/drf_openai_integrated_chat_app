dbba2b47fd79578066997a7348c896218ab1745f
suhail-chand <suhail.chand@outlook.com>
2024-02-21 12:07:12 +0530

    tiktoken implementation

--- ai_chats/views.py
+++ ai_chats/views.py
@@ -125,77 +125,115 @@ class GetModelResponse(GenericAPIView):
             return Response({"error": str(exception)}, status=status.HTTP_400_BAD_REQUEST)
 
 
-def stream_model_response(model, chat_id, chat_history, conversations):
-    response = {
-        "status_code": status.HTTP_200_OK,
-        "data": None,
-        "error": None,
-        "message": "SUCCESS"
-    }
-    try:
-        model_response = {"role": "assistant", "content": str()}
-        if model in chat_completion_models:
-            for chunk in openai.ChatCompletion.create(
-                    model=model,
-                    messages=chat_history + conversations,
-                    max_tokens=1024,
-                    stream=True
-            ):
-                model_response["content"] += chunk["choices"][0]["message"]["content"]
-                response["data"] = chunk["choices"][0]["message"]["content"]
-                yield json.dumps(response) + "\n"
-        else:
-            prompt = str()
-            for conversation in chat_history + conversations:
-                prompt += f"-{conversation['role'][0:1]}> {conversation['content']}\n"
-            prompt = prompt + '-a> '
+import tiktoken
 
-            for chunk in openai.Completion.create(
-                    model=model,
-                    prompt=prompt,
-                    max_tokens=1024,
-                    stream=True
-            ):
-                model_response["content"] += chunk["choices"][0]["text"]
-                response["data"] = chunk["choices"][0]["text"]
-                yield json.dumps(response) + "\n"
+AI_CHAT_SYSTEM_INSTRUCTION = "You are a friendly chat-bot who provides precise response to human queries."
 
-        conversations.append(model_response)
 
-        for conversation in conversations:
-            conversation.update({"chat": chat_id})
+def get_formatted_prompt(model_id, query, chat_history):
+    """
+    Function to format prompt based on model's API compatibility.
+    :param model_id: Open-AI model ID
+    :param query: Current user query
+    :param chat_history: Chat history to maintain context
+    :return: Formatted prompt
+    """
 
-        conversations_serialized = ConversationSerializer(data=conversations, many=True)
-        conversations_serialized.is_valid(raise_exception=True)
-        conversations_serialized.save()
+    # Tokenization algorithm as per the model ID
+    encoding = tiktoken.encoding_for_model(model_id)
 
-    except Exception as exception:
-        response.update({
-            "status_code": status.HTTP_400_BAD_REQUEST,
-            "error": str(exception),
-            "message": "FAILURE"
-        })
-        yield json.dumps(response) + "\n"
+    if model_id in completion_models:
+        prompt = [f'-s> {AI_CHAT_SYSTEM_INSTRUCTION} ',
+                  f'-u> {query["content"]} -a> ']
+    else:
+        prompt = [{'role': 'system', 'content': AI_CHAT_SYSTEM_INSTRUCTION},
+                  {'role': 'user', 'content': query['content']}]
+
+    # Get token count of the query
+    tokens_count = len(encoding.encode(AI_CHAT_SYSTEM_INSTRUCTION)) + len(encoding.encode(query['content']))
+
+    # Include chat history
+    for conversation in chat_history:
+        if conversation['role'] == 'assistant':
+            query = chat_history[chat_history.index(conversation) - 1]['content']
+            response = conversation['content']
+            conversation_tokens_count = len(encoding.encode(query)) + len(encoding.encode(response))
+
+            # Ensuring that the prompt does not consume the model's token limit of 4096
+            if (tokens_count + conversation_tokens_count) <= (4096 - 1000):
+                tokens_count += conversation_tokens_count
+                if model_id in completion_models:
+                    prompt.insert(1, f'-u> {query} ')
+                    prompt.insert(2, f'-a> {response} ')
+                else:
+                    prompt.insert(1, {'role': 'user', 'content': query})
+                    prompt.insert(2, {'role': 'assistant', 'content': response})
+            else:
+                break
+    print(prompt)
+    return prompt
+
+
+def stream_model_response(model_id, chat_id, current_conversation, chat_history):
+    streaming_response = str()
+    model_response = {"role": "assistant", "content": str()}
+    if model_id in completion_models:
+        for chunk in openai.Completion.create(
+                stream=True,
+                model=model_id,
+                prompt=get_formatted_prompt(model_id=model_id,
+                                            query=current_conversation[0],
+                                            chat_history=chat_history)
+        ):
+            model_response["content"] += chunk["choices"][0]["text"]
+            streaming_response = chunk["choices"][0]["text"]
+            yield json.dumps(streaming_response) + "\n"
+
+    else:
+        for chunk in openai.ChatCompletion.create(
+                stream=True,
+                model=model_id,
+                messages=get_formatted_prompt(model_id=model_id,
+                                              query=current_conversation[0],
+                                              chat_history=chat_history)
+        ):
+            model_response["content"] += chunk["choices"][0]["message"]["content"]
+            streaming_response = chunk["choices"][0]["message"]["content"]
+            yield json.dumps(streaming_response) + "\n"
+
+    current_conversation.append(model_response)
+
+    for instance in current_conversation:
+        instance.update({"chat": chat_id})
+
+    conversations_serialized = ConversationSerializer(data=current_conversation, many=True)
+    conversations_serialized.is_valid(raise_exception=True)
+    conversations_serialized.save()
 
 
 class StreamModelResponse(GenericAPIView):
     serializer_class = ChatConversationSerializer
 
     def get_queryset(self):
-        return Conversation.objects.filter(chat=self.kwargs["pk"]).order_by('id')
+        """
+        :return: 10 latest conversations
+        """
+        return Conversation.objects.filter(chat=self.kwargs["pk"]).order_by('-created_at')[:10]
 
     def post(self, request, **kwargs):
         if request.data["model"] in chat_completion_models + completion_models + fine_tunes:
             chat_history = self.get_serializer(self.get_queryset(), many=True).data
-            conversations = [{
+            current_conversation = [{
                 "role": "user", "content": request.data["content"]
             }]
 
-            return StreamingHttpResponse(stream_model_response(model=request.data["model"],
-                                                               chat_id=kwargs["pk"],
-                                                               chat_history=chat_history,
-                                                               conversations=conversations),
-                                         content_type='text/event-stream')
+            return StreamingHttpResponse(
+                stream_model_response(
+                    model_id=request.data["model"],
+                    chat_id=kwargs["pk"],
+                    current_conversation=current_conversation,
+                    chat_history=chat_history),
+                content_type='text/event-stream')
 
         else:
             return Response({"error": f"The model: `{request.data['model']}` does not exist"},
